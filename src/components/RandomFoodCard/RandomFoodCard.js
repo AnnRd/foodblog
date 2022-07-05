b409d3230215b97e41cb9fbf54ab5d7800612e4b
Anna Radchenko <anrd.rs>
2022-07-05 18:07:55 +0300

    fetch to find meal by category added

--- src/components/RandomFoodCard/RandomFoodCard.js
+++ src/components/RandomFoodCard/RandomFoodCard.js
@@ -2,8 +2,16 @@ import './randomFoodCard.scss';
 
 function RandomFoodCard (props) {
     const {img, name, key} = props;
+
+    const getCategory = async () => {
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${name}`);
+
+        const category = await response.json();
+        console.log(category);
+    }
+
     return (
-            <div className='card' id={key}>
+            <div onClick={getCategory} className='card' id={key} value={name}>
                 <img className='food-img' src={img} alt={name} />
                 <div className='food-name'>{name}</div>
             </div>
