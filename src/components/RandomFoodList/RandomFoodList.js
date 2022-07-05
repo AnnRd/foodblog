5fd82bb83393d351ddc5b7bf9e1ca333d360639c
Anna Radchenko <anrd.rs>
2022-07-05 16:31:44 +0300

    fetch added

--- src/components/RandomFoodList/RandomFoodList.js
+++ src/components/RandomFoodList/RandomFoodList.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
 import RandomFoodCard from "../RandomFoodCard/RandomFoodCard";
 import './randomFoodList.scss';
 
 function RandomFoodList ({data}) {
+    const [foodList, setFoodList] = useState([]);
+
+    const getMeals = async () => {
+        const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+
+        const data = await response.json();
+        console.log(data);
+    }
+
+    useEffect(() => {
+        getMeals()
+    }, []);
+
+    // useEffect(() => {
+    //     fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list')
+    //         .then(res=>res.json())
+    //         .then(data=> console.log(data))
+    // }, []);
+
+
+
     return (
         <div className="list">
             {data.map((dish) => {
