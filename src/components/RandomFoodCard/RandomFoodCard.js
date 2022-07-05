ce5da9228b3416e70296d6a0d5b53320e4ee7bb7
Anna Radchenko <anrd.rs>
2022-07-05 13:39:50 +0300

    styles updated

--- src/components/RandomFoodCard/RandomFoodCard.js
+++ src/components/RandomFoodCard/RandomFoodCard.js
@@ -2,14 +2,11 @@ import './randomFoodCard.scss';
 
 function RandomFoodCard (props) {
     const {img, name} = props;
-    // const foodList = 
     return (
-        // <div className='list'>
             <div className='card'>
                 <img className='food-img' src={img} alt="" />
                 <div className='food-name'>{name}</div>
             </div>
-        //{/* </div> */}
     )
 }
 
