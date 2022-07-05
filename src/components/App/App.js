efe1770bc4570a999de1b351b8cc8bbcae5fcd43
Anna Radchenko <anrd.rs>
2022-07-05 17:04:14 +0300

    fetch fixed and state mapped

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,31 +9,6 @@ import UpdateButton from "../UpdateButton/UpdateButton";
 
 
 function App() {
-  const data = [{
-    img: "https://www.themealdb.com/images/media/meals/pn59o51628769837.jpg",
-    name: 'Bludo1'
-  }, {
-    img: "https://www.themealdb.com/images/media/meals/pn59o51628769837.jpg",
-    name: 'Bludo2'
-  }, {
-    img: "https://www.themealdb.com/images/media/meals/pn59o51628769837.jpg",
-    name: 'Bludo3'
-  }, {
-    img: "https://www.themealdb.com/images/media/meals/pn59o51628769837.jpg",
-    name: 'Bludo4'
-  }, {
-    img: "https://www.themealdb.com/images/media/meals/pn59o51628769837.jpg",
-    name: 'Bludo5'
-  }, {
-    img: "https://www.themealdb.com/images/media/meals/pn59o51628769837.jpg",
-    name: 'Bludo6'
-  }, {
-    img: "https://www.themealdb.com/images/media/meals/pn59o51628769837.jpg",
-    name: 'Bludo7'
-  }, {
-    img: "https://www.themealdb.com/images/media/meals/pn59o51628769837.jpg",
-    name: 'Bludo8'
-  }]
 
   return (
     <div className="App">
@@ -44,7 +19,7 @@ function App() {
         {/* <Menu/> */}
         <FoodFinder/>
         <div>
-          <RandomFoodList data={data}/>
+          <RandomFoodList/>
         </div>
         <UpdateButton/>
       </main>
--- src/components/RandomFoodList/RandomFoodList.js
+++ src/components/RandomFoodList/RandomFoodList.js
@@ -2,32 +2,27 @@ import { useEffect, useState } from "react";
 import RandomFoodCard from "../RandomFoodCard/RandomFoodCard";
 import './randomFoodList.scss';
 
-function RandomFoodList ({data}) {
+function RandomFoodList () {
     const [foodList, setFoodList] = useState([]);
 
     const getMeals = async () => {
-        const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+        const response = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
 
-        const data = await response.json();
-        console.log(data);
+        const food = await response.json();
+        setFoodList(food);
     }
 
     useEffect(() => {
-        getMeals()
+        getMeals();
     }, []);
 
-    // useEffect(() => {
-    //     fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list')
-    //         .then(res=>res.json())
-    //         .then(data=> console.log(data))
-    // }, []);
-
+    console.log(foodList);
 
 
     return (
         <div className="list">
-            {data.map((dish) => {
-            return <RandomFoodCard name={dish.name} img={dish.img}/>
+            {foodList.categories?.map((dish) => {
+            return <RandomFoodCard name={dish.strCategory} img={dish.strCategoryThumb} key={dish.idCategory}/>
         })}
         </div>
     )
