4f4ad0a889610ba1bdf5902ee661a6a8afb6ea8f
Anna Radchenko <anrd.rs>
2022-07-05 17:22:07 +0300

    form changed

--- src/components/FoodFinder/FoodFinder.js
+++ src/components/FoodFinder/FoodFinder.js
@@ -2,14 +2,12 @@ import './foodFinder.scss';
 
 function FoodFinder () {
     return (
-        <>
-            <form action="">
+        <div className='finder'>
                 <label className="finder__label" htmlFor="GET-food">Find your dish</label> <br />
                 <input className="finder__input" id="GET-food" type="text" placeholder="I want ..."/> 
                 <input className="finder__submit" type="submit" value="Find"></input>
-            </form>
             <div className="finder__label">... or check up the categories! 👇</div>
-        </>
+        </div>
     )
 }
 
